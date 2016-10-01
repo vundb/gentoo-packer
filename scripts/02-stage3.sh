53b1df803e8f3a6c83691be707cfdb06dc0e0c6a
Jonathan Alainis <jonathan@alainis.de>
2016-10-01 23:05:52 +0200

    Switched to multilib and curl

--- scripts/02-stage3.sh
+++ scripts/02-stage3.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-tarball=stage3-amd64-nomultilib-$STAGE3.tar.bz2
+tarball=stage3-amd64-$STAGE3.tar.bz2
 
 mount /dev/sda4 /mnt/gentoo
 
 cd /mnt/gentoo
-wget http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE3/$tarball
-tar xvjpf $tarball
-rm -f $tarball
+curl -sL http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE3/$tarball | tar xvjp
