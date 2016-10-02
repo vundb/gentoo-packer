8013d92bf246758bfc1828850de9f58068d616df
Jonathan Alainis <jonathan@alainis.de>
2016-10-02 10:38:46 +0200

    Provide custom kernel
    Only tested for VirtualBox

--- scripts/08-kernel.sh
+++ scripts/08-kernel.sh
@@ -4,9 +4,10 @@ cp $SCRIPTS/scripts/kernel.config /mnt/gentoo/tmp/
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
 emerge sys-kernel/gentoo-sources
-emerge sys-kernel/genkernel
 cd /usr/src/linux
 mv /tmp/kernel.config .config
-genkernel --install --symlink --no-zfs --no-btrfs --oldconfig all
-emerge -c sys-kernel/genkernel
+make
+make modules_install
+make install
+emerge -C sys-kernel/gentoo-sources
 EOF
