9aa880718d050b4ebacd7b00ead6a540e4ac8028
Jonathan Alainis <jonathan@alainis.de>
2016-10-02 10:17:58 +0200

    Nonverbose stage3 untar

--- scripts/02-stage3.sh
+++ scripts/02-stage3.sh
@@ -5,4 +5,4 @@ tarball=stage3-amd64-$STAGE3.tar.bz2
 mount /dev/sda4 /mnt/gentoo
 
 cd /mnt/gentoo
-curl -sL http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE3/$tarball | tar xvjp
+curl -sL http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE3/$tarball | tar xjp
