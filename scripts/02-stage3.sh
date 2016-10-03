efa439caf6f611d6d2bfdb458b85e5cc51f26363
Jonathan Alainis <jonathan@alainis.de>
2016-10-03 18:35:41 +0200

    Added stage3 mirrorselect

--- scripts/02-stage3.sh
+++ scripts/02-stage3.sh
@@ -4,5 +4,7 @@ tarball=stage3-amd64-$STAGE3.tar.bz2
 
 mount /dev/sda4 /mnt/gentoo
 
+MIRROR=`mirrorselect -b10 -D -s1 -o -H -c 'Germany' | grep 'GENTOO_MIRRORS='`
+
 cd /mnt/gentoo
-curl -sL http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE3/$tarball | tar xjp
+curl -L --progress-bar --retry 4 --retry-delay 1 ${MIRROR:16:-1}releases/amd64/autobuilds/$STAGE3/$tarball | tar xjp
