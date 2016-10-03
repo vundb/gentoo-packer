8c66f214fb966034fbf85b75a260ce5ef31163e8
Jonathan Alainis <jonathan@alainis.de>
2016-10-03 19:06:03 +0200

    Fixed curl command

--- scripts/02-stage3.sh
+++ scripts/02-stage3.sh
@@ -7,4 +7,6 @@ mount /dev/sda4 /mnt/gentoo
 MIRROR=`mirrorselect -b10 -D -s1 -o -H -c 'Germany' | grep 'GENTOO_MIRRORS='`
 
 cd /mnt/gentoo
-curl -L --progress-bar --retry 4 --retry-delay 1 ${MIRROR:16:-1}releases/amd64/autobuilds/$STAGE3/$tarball | tar xjp
+curl -LO --progress-bar --retry 4 --retry-delay 1 ${MIRROR:16:-1}releases/amd64/autobuilds/$STAGE3/$tarball
+tar xjpf $tarball
+rm $tarball
