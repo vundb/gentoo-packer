a7081c792066502e20615efb504559e69fb296cc
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-26 16:12:17 -0400

    This gets deleted in cleanup.sh

--- scripts/05-portage.sh
+++ scripts/05-portage.sh
@@ -5,5 +5,4 @@ chroot /mnt/gentoo /bin/bash <<'EOF'
 mkdir /usr/portage
 emerge-webrsync -v -q
 echo "MAKEOPTS=\"-j3\" >> /etc/portage/make.conf
-echo "masters = gentoo" >> /usr/portage/metadata/layout.conf
 EOF
