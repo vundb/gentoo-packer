f527ba69f8352bda3d983873ef7af9b7585a6a65
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-27 09:39:55 -0400

    Add vagrant to the docker group

--- scripts/15-tools.sh
+++ scripts/15-tools.sh
@@ -3,5 +3,7 @@
 chroot /mnt/gentoo /bin/bash <<'EOF'
 echo ">=sys-libs/libseccomp-2.3.0 static-libs" > /etc/portage/package.use/docker
 emerge -vq app-emulation/docker
+groupadd docker
+usermod -a -G docker vagrant
 rc-update add docker default
 EOF
