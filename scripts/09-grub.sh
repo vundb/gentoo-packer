ef5ba16542f27e102b629833f6bc3611e1d9bfeb
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-27 12:22:42 -0400

    Update 09-grub.sh

--- scripts/09-grub.sh
+++ scripts/09-grub.sh
@@ -3,6 +3,7 @@
 chroot /mnt/gentoo /bin/bash <<'EOF'
 emerge -vq sys-boot/grub
 grub-install /dev/sda
-echo "GRUB_TIMEOUT=0" >> /etc/default/grub
+echo "GRUB_HIDDEN_TIMEOUT=0" >> /etc/default/grub
+echo "GRUB_HIDDEN_TIMEOUT_QUIET=true" >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
