b652a5c18fa747c2bc761404b9bfe15613a5d66a
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-27 12:37:27 -0400

    Overcome a grub bug
    https://bugs.launchpad.net/ubuntu/+source/grub2/+bug/1273764/comments/22

--- scripts/09-grub.sh
+++ scripts/09-grub.sh
@@ -3,7 +3,6 @@
 chroot /mnt/gentoo /bin/bash <<'EOF'
 emerge -vq sys-boot/grub
 grub-install /dev/sda
-echo "GRUB_HIDDEN_TIMEOUT=0" >> /etc/default/grub
-echo "GRUB_HIDDEN_TIMEOUT_QUIET=true" >> /etc/default/grub
+echo "GRUB_TIMEOUT=0.1" >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
