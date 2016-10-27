a2a821bc83c937ef7ac1d28f64c9466432770e6b
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-26 21:15:33 -0400

    Change to US keymap

--- scripts/14-keymap.sh
+++ scripts/14-keymap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
-  sed -i "s/keymap=\".*\"/keymap=\"de\"/" /etc/conf.d/keymaps
+  sed -i "s/keymap=\".*\"/keymap=\"us\"/" /etc/conf.d/keymaps
 EOF
