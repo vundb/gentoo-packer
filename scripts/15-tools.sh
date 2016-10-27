9df506fda440161bd7b5e59cf38c5a5e4794557b
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-27 06:42:40 -0400

    Update 15-tools.sh

--- scripts/15-tools.sh
+++ scripts/15-tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
+echo ">=sys-libs/libseccomp-2.3.0 static-libs" > /etc/portage/package.use/docker
 emerge -vq app-emulation/docker
 rc-update add docker default
 EOF
