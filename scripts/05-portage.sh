6e2a21b35cdc30189c8c5164a7e34fd62d948af6
Chris Crawford <christopher.p.crawford@gmail.com>
2016-11-01 12:53:37 -0400

    Fix a typo

--- scripts/05-portage.sh
+++ scripts/05-portage.sh
@@ -4,5 +4,5 @@ echo 'Sync portage'
 chroot /mnt/gentoo /bin/bash <<'EOF'
 mkdir /usr/portage
 emerge-webrsync -v -q
-echo "MAKEOPTS=\"-j3\" >> /etc/portage/make.conf
+echo "MAKEOPTS=\"-j3\"" >> /etc/portage/make.conf
 EOF
