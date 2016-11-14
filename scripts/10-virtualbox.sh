5c18bdbc22c0c61d03ae61ced1481c443e249477
Peter <silmano@gmail.com>
2016-11-14 16:00:08 +0100

    Added latest version of Guest Additions

--- scripts/10-virtualbox.sh
+++ scripts/10-virtualbox.sh
@@ -3,9 +3,9 @@
 chroot /mnt/gentoo /bin/bash <<'EOF'
 emerge -vq "=virtual/linux-sources-1"
 
-emerge -vq  ">=app-emulation/virtualbox-guest-additions-5.0.20" --autounmask-write
+emerge -vq  ">=app-emulation/virtualbox-guest-additions-5.1.8" --autounmask-write
 etc-update --automode -5
-emerge -vq  ">=app-emulation/virtualbox-guest-additions-5.0.20"
+emerge -vq  ">=app-emulation/virtualbox-guest-additions-5.1.8"
 
 rc-update add virtualbox-guest-additions default
 EOF
