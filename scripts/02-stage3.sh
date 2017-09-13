1ebad56bb31deb51b65a390b2c12b9d4a8bed435
codylane <cody.lane@gmail.com>
2017-09-13 11:01:12 -0600

    adding failsafe mirror selection

--- scripts/02-stage3.sh
+++ scripts/02-stage3.sh
@@ -12,6 +12,9 @@ MIRROR=$(grep '^GENTOO_MIRRORS' '/etc/portage/make.conf')
 [ -z $MIRROR ] && MIRROR=`mirrorselect -b10 -s1 -o -H -t60 -c $COUNTRY | grep 'GENTOO_MIRRORS='`
 MIRROR=$(echo $MIRROR | sed -e 's/"//g')
 
+# incase we were unable to find a mirror
+[ -z "${MIRROR}" ] && MIRROR='GENTOO_MIRRORS=http://mirrors.evowise.com/gentoo/'
+
 cd /mnt/gentoo
 echo 'Download stage3'
 curl -sL --progress-bar --retry 4 --retry-delay 1 "${MIRROR##GENTOO_MIRRORS=}/releases/amd64/autobuilds/${STAGE3}/${tarball}" | tar xpj
