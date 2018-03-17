3c485e5e9fd97a733c6651d3448d908850f7fc6f
codylane <cody.lane@gmail.com>
2018-03-17 10:16:29 -0600

    fixes due to updatesm changing tar format to xz instead of bzip2

--- scripts/02-stage3.sh
+++ scripts/02-stage3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -uex
 
 ARCH="${ARCH:-amd64}"
-tarball=stage3-amd64-$STAGE3.tar.bz2
-[ $ARCH == "x86" ] && tarball=stage3-i686-$STAGE3.tar.bz2 || true
+tarball=stage3-amd64-$STAGE3.tar.xz
+[ $ARCH == "x86" ] && tarball=stage3-i686-$STAGE3.tar.xz || true
 
 mkdir -p /mnt/gentoo
 mount -o rw /dev/sda4 /mnt/gentoo
@@ -19,5 +19,5 @@ MIRROR=${MIRROR:-'GENTOO_MIRRORS=http://distfiles.gentoo.org'}
 cd /mnt/gentoo
 stage3_url="${MIRROR##GENTOO_MIRRORS=}/releases/$ARCH/autobuilds/${STAGE3}/${tarball}"
 echo "Download stage3 from $stage3_url"
-curl -sL --progress-bar --retry 4 --retry-delay 1 "$stage3_url" | tar xpj
+curl -sL --progress-bar --retry 4 --retry-delay 1 "$stage3_url" | tar xpJ
 ls -l
