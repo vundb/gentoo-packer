5276da740b2a660b883deb9bfb53eefc9ffecc50
codylane <cody.lane@gmail.com>
2018-03-17 08:48:15 -0600

    per https://github.com/kimb/gentoo-packer/commit/bd4c5168899e4e9b37630be6fd684babf11a8f8a

--- scripts/02-stage3.sh
+++ scripts/02-stage3.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+ARCH="${ARCH:-amd64}"
 tarball=stage3-amd64-$STAGE3.tar.bz2
+[ $ARCH == "x86" ] && tarball=stage3-i686-$STAGE3.tar.bz2
 
 mkdir -p /mnt/gentoo
 mount -o rw /dev/sda4 /mnt/gentoo
 
-COUNTRY="${COUNTRY:-USA}"
-
 echo 'Testing stage3 mirrors'
-MIRROR=$(grep '^GENTOO_MIRRORS' '/etc/portage/make.conf')
-[ -z $MIRROR ] && MIRROR=`mirrorselect -b10 -s1 -o -H -t60 -c $COUNTRY | grep 'GENTOO_MIRRORS='`
+MIRROR=${MIRROR:-$(grep '^GENTOO_MIRRORS' '/etc/portage/make.conf' | sed -e 's/\s.*$//' )}
+flags="${MIRRORSELECT_REGION:+-R $MIRRORSELECT_REGION} ${MIRRORSELECT_COUNTRY:+-c $MIRRORSELECT_COUNTRY}"
+MIRROR=${MIRROR:-$(mirrorselect -b10 -s1 -o -H -t60 $flags | grep 'GENTOO_MIRRORS=')}
 MIRROR=$(echo $MIRROR | sed -e 's/"//g')
 
 # incase we were unable to find a mirror
-[ -z "${MIRROR}" ] && MIRROR='GENTOO_MIRRORS=http://distfiles.gentoo.org'
+MIRROR=${MIRROR:-'GENTOO_MIRRORS=http://distfiles.gentoo.org'}
 
 cd /mnt/gentoo
-echo 'Download stage3'
-curl -sL --progress-bar --retry 4 --retry-delay 1 "${MIRROR##GENTOO_MIRRORS=}/releases/amd64/autobuilds/${STAGE3}/${tarball}" | tar xpj
+stage3_url="${MIRROR##GENTOO_MIRRORS=}/releases/$ARCH/autobuilds/${STAGE3}/${tarball}"
+echo "Download stage3 from $stage3_url"
+curl -sL --progress-bar --retry 4 --retry-delay 1 "$stage3_url" | tar xpj
 ls -l
--- scripts/08-kernel.sh
+++ scripts/08-kernel.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 cp $SCRIPTS/scripts/kernel.config /mnt/gentoo/tmp/
+if [ "$ARCH" == "x86" ]; then
+    sed -i 's/CONFIG_64BIT=y/CONFIG_64BIT=n/' /mnt/gentoo/tmp/kernel.config
+    sed -i 's/CONFIG_TRIM_UNUSED_KSYMS=y/CONFIG_TRIM_UNUSED_KSYMS=n/' /mnt/gentoo/tmp/kernel.config
+fi
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
 emerge -vq sys-kernel/gentoo-sources
 cd /usr/src/linux
 mv /tmp/kernel.config .config
+make kvmconfig
+make listnewconfig
+make olddefconfig
 echo 'make'
 make -s
 echo 'make modules_install'
