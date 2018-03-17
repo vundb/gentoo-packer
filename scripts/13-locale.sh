4651cf1b191ead4177c6395b61a8ff5977b5f414
codylane <cody.lane@gmail.com>
2018-03-17 08:40:45 -0600

    per https://github.com/kimb/gentoo-packer/commit/307c931fbfa473ebc86279497d48793c06b6db81

--- scripts/13-locale.sh
+++ scripts/13-locale.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+LANG="${LANG:-en_US}"
+
 chroot /mnt/gentoo /bin/bash <<'EOF'
 cat > /etc/env.d/02locale <<'LOC'
-LANG="en_US.UTF-8"
-LC_ALL="en_US.UTF-8"
-LC_LANG="en_US.UTF-8"
+LANG="${LANG}.UTF-8"
+LC_COLLATE=C
 LOC
-sed -i "s/#en_US\.UTF-8/en_US\.UTF-8/" /etc/locale.gen
+sed -i "s/#en_US\.UTF-8/${LANG}\.UTF-8/" /etc/locale.gen
 locale-gen
 EOF
