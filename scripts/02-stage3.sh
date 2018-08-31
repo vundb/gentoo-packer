9a116652607e4dd2a2d11f62f734a80423dc9529
Viktor Breitenbücher <vic.b@outlook.com>
2018-08-31 08:01:09 +0200

    Update project for custom purposes.

--- scripts/02-stage3.sh
+++ scripts/02-stage3.sh
@@ -10,7 +10,7 @@ mount -o rw /dev/sda4 /mnt/gentoo
 echo 'Testing stage3 mirrors'
 MIRROR=${MIRROR:-$(grep '^GENTOO_MIRRORS' '/etc/portage/make.conf' | sed -e 's/\s.*$//' )}
 flags="${MIRRORSELECT_REGION:+-R $MIRRORSELECT_REGION} ${MIRRORSELECT_COUNTRY:+-c $MIRRORSELECT_COUNTRY}"
-MIRROR=${MIRROR:-$(mirrorselect -b10 -s1 -o -H -t60 $flags | grep 'GENTOO_MIRRORS=')}
+MIRROR=${MIRROR:-$(mirrorselect -b10 -s1 -D -o -H -t60 $flags | grep 'GENTOO_MIRRORS=')}
 MIRROR=$(echo $MIRROR | sed -e 's/"//g')
 
 # incase we were unable to find a mirror
