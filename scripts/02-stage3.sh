0db2a132cec5a243625d4830da1d3fe9860f1705
Viktor Breitenbücher <v.breitenbuecher@shopmacher.de>
2019-02-22 14:04:20 +0700

    Stop checking for mirrorselect

--- scripts/02-stage3.sh
+++ scripts/02-stage3.sh
@@ -7,11 +7,11 @@ tarball=stage3-amd64-$STAGE3.tar.xz
 mkdir -p /mnt/gentoo
 mount -o rw /dev/sda4 /mnt/gentoo
 
-echo 'Testing stage3 mirrors'
-MIRROR=${MIRROR:-$(grep '^GENTOO_MIRRORS' '/etc/portage/make.conf' | sed -e 's/\s.*$//' )}
-flags="${MIRRORSELECT_REGION:+-R $MIRRORSELECT_REGION} ${MIRRORSELECT_COUNTRY:+-c $MIRRORSELECT_COUNTRY}"
-MIRROR=${MIRROR:-$(mirrorselect -b10 -s1 -D -o -H -t60 $flags | grep 'GENTOO_MIRRORS=')}
-MIRROR=$(echo $MIRROR | sed -e 's/"//g')
+#echo 'Testing stage3 mirrors'
+#MIRROR=${MIRROR:-$(grep '^GENTOO_MIRRORS' '/etc/portage/make.conf' | sed -e 's/\s.*$//' )}
+#flags="${MIRRORSELECT_REGION:+-R $MIRRORSELECT_REGION} ${MIRRORSELECT_COUNTRY:+-c $MIRRORSELECT_COUNTRY}"
+#MIRROR=${MIRROR:-$(mirrorselect -b10 -s1 -D -o -H -t60 $flags | grep 'GENTOO_MIRRORS=')}
+#MIRROR=$(echo $MIRROR | sed -e 's/"//g')
 
 # incase we were unable to find a mirror
 MIRROR=${MIRROR:-'GENTOO_MIRRORS=http://distfiles.gentoo.org'}
